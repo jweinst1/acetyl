6218298cd59c7d1073f74439357427bcbb5374bd
Joshua Weinstein <jweinstein@splunk.com>
2019-07-08 23:57:14 -0700

    element work

--- src/AcetylElement.cpp
+++ src/AcetylElement.cpp
@@ -2,23 +2,59 @@
 
 AcetylElement::AcetylElement(): _type(ACETYL_ELEM_TYPE_NONE)
 {
-	// nothing
+	clearId();
 }
 
-AcetylElement::AcetylElement(bool boolean): _type(ACETYL_ELEM_TYPE_BOOL)
+AcetylElement::AcetylElement(const char* id): _type(ACETYL_ELEM_TYPE_NONE)
 {
+	clearId();
+	std::strncpy(_id, id, ACETYL_ELEMENT_ID_MAX_CHARS);
+}
+
+AcetylElement::AcetylElement(const char* id, bool boolean): _type(ACETYL_ELEM_TYPE_BOOL)
+{
+	clearId();
 	_boolean = boolean;
+	std::strncpy(_id, id, ACETYL_ELEMENT_ID_MAX_CHARS);
 }
 
 
-AcetylElement::AcetylElement(double number): _type(ACETYL_ELEM_TYPE_NUMBER)
+AcetylElement::AcetylElement(const char* id, double number): _type(ACETYL_ELEM_TYPE_NUMBER)
 {
+	clearId();
 	_number = number;
+	std::strncpy(_id, id, ACETYL_ELEMENT_ID_MAX_CHARS);
 }
 
-AcetylElement::AcetylElement(AcetylElement* joined): _type(ACETYL_ELEM_TYPE_JOINED)
+AcetylElement::AcetylElement(const char* id, AcetylElement* joined): _type(ACETYL_ELEM_TYPE_JOINED)
 {
+	clearId();
 	_joined = joined;
+	std::strncpy(_id, id, ACETYL_ELEMENT_ID_MAX_CHARS);
+}
+
+AcetylElement& AcetylElement::operator=(const AcetylElement& other)
+{
+	if(&other == this)
+		return *this;
+	else {
+		_type = other._type;
+		switch(_type) {
+			case ACETYL_ELEM_TYPE_NONE:
+			   break;
+			case ACETYL_ELEM_TYPE_BOOL:
+			   _boolean = other._boolean;
+			   break;
+			case ACETYL_ELEM_TYPE_NUMBER:
+			   _number = other._number;
+			   break;
+			case ACETYL_ELEM_TYPE_JOINED:
+			   _joined = other._joined;
+			   break;
+		}
+		std::strncpy(_id, other._id, ACETYL_ELEMENT_ID_MAX_CHARS);
+		return *this;
+	}
 }
 
 AcetylElement::AcetylElement(const AcetylElement& other): _type(other._type)
@@ -35,5 +71,26 @@ AcetylElement::AcetylElement(const AcetylElement& other): _type(other._type)
 		case ACETYL_ELEM_TYPE_JOINED:
 		   _joined = other._joined;
 		   break;
+	}
+	std::strncpy(_id, other._id, ACETYL_ELEMENT_ID_MAX_CHARS);
+}
+
+void AcetylElement::clearId()
+{
+	for(unsigned i = 0; i < ACETYL_ELEMENT_ID_MAX_CHARS + 1 ; i++)
+		_id[i] = '\0';
+}
+
+bool AcetylElement::boolean() const
+{
+	switch(_type) {
+		case ACETYL_ELEM_TYPE_BOOL:
+		    return _boolean;
+		case ACETYL_ELEM_TYPE_NUMBER:
+		    return _number != 0.0;
+		case ACETYL_ELEM_TYPE_NONE:
+		    return false;
+		case ACETYL_ELEM_TYPE_JOINED:
+		    return _joined != nullptr;
 	}
 }
--- src/AcetylElement.h
+++ src/AcetylElement.h
@@ -2,6 +2,11 @@
 #define SRC_ACETYL_ELEMENT_H
 
 #include <iostream>
+#include <cstring>
+
+#ifndef ACETYL_ELEMENT_ID_MAX_CHARS
+#define ACETYL_ELEMENT_ID_MAX_CHARS 25
+#endif
 
 
 enum AcetylElementType
@@ -16,9 +21,10 @@ class AcetylElement
 {
 public:
 	AcetylElement();
-	AcetylElement(bool boolean);
-	AcetylElement(double number);
-	AcetylElement(AcetylElement* joined);
+	AcetylElement(const char* id);
+	AcetylElement(const char* id, bool boolean);
+	AcetylElement(const char* id, double number);
+	AcetylElement(const char* id, AcetylElement* joined);
 	~AcetylElement()
 	{
 		// in the joined type, pointers are not owned
@@ -27,9 +33,22 @@ public:
 
 	AcetylElement(const AcetylElement& other);
 
+	AcetylElement& operator=(const AcetylElement& other);
+
 	AcetylElementType getType() const { return _type; }
 
+	const char* getId() const { return _id; }
+
+	void setId(const char* id) { std::strncpy(_id, id, ACETYL_ELEMENT_ID_MAX_CHARS); }
+
+	bool isId(const char* id) const { return std::strncmp(id, _id, ACETYL_ELEMENT_ID_MAX_CHARS) == 0; }
+
+	bool boolean() const;
+
 private:
+	void clearId();
+private:
+	char _id[ACETYL_ELEMENT_ID_MAX_CHARS + 1];
 	AcetylElementType _type;
 	union 
 	{
