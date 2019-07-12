5298629eef347c77a371b7df8818814ae1d0ca28
Joshua Weinstein <jweinstein@splunk.com>
2019-07-11 17:22:18 -0700

    op type rules

--- src/AcetylRule.h
+++ src/AcetylRule.h
@@ -1,6 +1,26 @@
 #ifndef SRC_ACETYL_RULE_H
 #define SRC_ACETYL_RULE_H
 
+#include <algorithm>
 
+enum AcetylRulePartType
+{
+	ACETYL_RULEPART_TYPE_PASS,
+	ACETYL_RULEPART_TYPE_FAIL,
+	ACETYL_RULEPART_TYPE_MODIFY
+};
+
+
+enum AcetylRulePartOp
+{
+	ACETYL_RULEPART_OP_HAS_ID
+};
+
+
+struct AcetylRulePart
+{
+	AcetylRulePartType type;
+	AcetylRulePartOp op;
+};
 
 #endif
